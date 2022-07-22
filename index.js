c37138eb1f646e898f341e376cc3598b6cab27e2
Katia <katia.punter@storyshareplatform.com>
2022-07-22 16:44:26 +0100

    Adds test set up and tests

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ document.getElementById('percent--form').addEventListener('submit', e => {
     e.preventDefault();
     const amount = document.getElementById('amount').value;
     const percent = document.getElementById('percent').value;
-    console.log(calculateResults(amount, percent));
+    const result = document.getElementById('result');
+    result.value = calculateResults(amount, percent);
 });
